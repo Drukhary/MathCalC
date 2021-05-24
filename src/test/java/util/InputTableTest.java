f7dea91f17c03e6a798135fe2ee8450b6578ac8a
FromItmo <FromItmo@ITMO.ITMO>
2021-05-24 17:55:11 +0300

    REPARI POWERFUL

---
+++ src/test/java/util/InputTableTest.java
@@ -0,0 +1,19 @@
+package util;
+
+import model.Point;
+import org.junit.Test;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class InputTableTest {
+    @Test
+    @org.junit.jupiter.api.Test
+    public void fileTest() throws FileNotFoundException {
+//        InputTable.approximateTableFunction();
+//        System.out.println(table);
+    }
+
+}
